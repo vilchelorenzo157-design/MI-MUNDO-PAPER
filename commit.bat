dbe9767fcae002a210aa28065d5e717d1b408179
vilchelorenzo157-design <vilchelorenzo157@gmail.com>
2025-12-03 02:06:14 -0300

    Actualizar estructura HTML de paginas

---
+++ commit.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "c:\Users\usuario\Desktop\MiMundoPaper"
+git add pages\index.html pages\productos.html pages\portfolio.html pages\reseñas.html
+git commit -m "Actualizar estructura HTML de paginas"
+git push origin nueva-funcionalidad
+pause
