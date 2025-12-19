d3797da7431155794edbf16c4cc91380f664ff29
vilchelorenzo157-design <vilchelorenzo157@gmail.com>
2025-12-19 18:50:47 -0300

    Fix navigation links in pages, remove duplicate index.html, update styles

--- push-changes.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-Set-Location "c:\Users\usuario\Desktop\MiMundoPaper"
-& git add pages/index.html pages/productos.html pages/portfolio.html "pages/reseñas.html"
-& git commit -m "Actualizar estructura HTML de paginas"
-& git push origin nueva-funcionalidad
-Write-Host "Commit completado exitosamente"
